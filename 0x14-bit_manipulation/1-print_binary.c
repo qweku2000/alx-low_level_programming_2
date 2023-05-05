6a8c77e556feb5f60f9777d128f7dd415d3228b8
root <joeadofonyarko@gmail.com>
2023-05-05 09:39:03 -0700

    Task 2

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ *print_binary - Print the binary of a number
+ *
+ *@n:number to be printed in binary
+*/
+
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	  {
+		print_binary(n >> 1);
+	  }
+
+	a = (n & 1) + '0';
+	putchar(a);
+}
