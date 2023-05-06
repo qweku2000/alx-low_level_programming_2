73da4c9af6ae038108e1b96198844d5bd0e5039d
root <joeadofonyarko@gmail.com>
2023-05-05 18:52:53 -0700

    Task 5

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,3 +1,4 @@
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
