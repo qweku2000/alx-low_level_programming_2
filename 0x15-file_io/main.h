02889c2ce1fa09b97b6d98af591f09ddae8f137d
root <joeadofonyarko@gmail.com>
2023-05-09 07:09:08 -0700

    Task 4

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,3 +1,4 @@
+int copy(char *file_from,char *file_to);
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
