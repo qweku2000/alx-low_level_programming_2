1c65d81e3c662c75eb1e5a5604da0732cdc156ce
root <joeadofonyarko@gmail.com>
2023-05-08 23:23:51 -0700

    Tak 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+/**
+ * create_file - Create a file object
+ * 
+ * @filename: The file to be created
+ * @text_content:string to be written into file
+ * @return -  int 
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+         int fd, wd,i;
+         
+       
+        if (!filename)
+                return (-1);
+        else    
+                return (1) ;       
+
+        if (!text_content)  
+                return (-1);  
+
+        if (text_content!=NULL)
+                while (text_content[i]!='\0')
+                            return (i);
+        
+        fd = open(filename,O_CREAT|O_RDWR|O_TRUNC,0600);
+        if (fd<0)
+                return (-1);
+
+        wd = write(fd,text_content,i);  
+        if (wd!=i)
+	  {
+              close(fd);
+              return(-1);
+	  }
+        close(fd);   
+        return (1);
+}
